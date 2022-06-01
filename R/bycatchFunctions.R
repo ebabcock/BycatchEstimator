b5d2c42373ca292ce5a0d0bbfd57739928d1f25d
Elizabeth Babcock <ebabcock@rsmas.miami.edu>
2022-06-01 07:08:11 -0400

    Update bycatchFunctions.R

--- R/bycatchFunctions.R
+++ R/bycatchFunctions.R
@@ -531,7 +531,7 @@ makePredictionsDeltaVar<-function(modfit1, newdat, modtype,  obsdatval, includeO
       obsdatvalyear=obsdatval[obsdatval$Year==years[i],]
       d=match(newdatall$matchColumn,obsdatvalyear$matchColumn)
       #d=match(logdat$matchColumn,obsdatvalyear$matchColumn)
-      d=a[!is.na(d)]
+      d=d[!is.na(d)]
       predval[d]= predval[d] + obsdatvalyear$Catch
     }
     if(includeObsCatch & modtype=="Binomial") {
