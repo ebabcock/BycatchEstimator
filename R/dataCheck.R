4060a777b527c35820910af802f8f42837daa56b
Elizabeth Babcock <ebabcock@rsmas.miami.edu>
2024-07-03 17:07:03 -0400

    Testing new data plots

---
+++ R/dataCheck.R
@@ -0,0 +1,43 @@
+
+
+#---------------------------------
+#Data Checking
+#----------------------------------
+
+#Roxygen header
+#'Make plots and tables of the data
+#'
+#'
+#'
+#' @param setupObj  An object produced by \code{bycatchSetup}.
+#' @import
+#' @export
+#' @keywords Data plotting function
+#' @examples
+#' \dontrun{
+#' library(BycatchEstimator)
+#' #-------------------------------------------------
+
+dataCheck<-function(setupObj){
+  #Setup directory naming
+  outDir<-paste0(setupObj$bycatchInputs$baseDir, paste("/Output", setupObj$bycatchInputs$runName))
+  if(!dir.exists(outDir)) dir.create(outDir)
+  fileName<-paste0(outDir,"/", Sys.Date(),"_BycatchModelSpecification.rds")
+  if(!file.exists(fileName))
+    saveRDS(output, file=fileName)
+  mkd<-tryCatch({
+    system.file("Markdown", "PrintDataChecks.Rmd",
+                package = "BycatchEstimator", mustWork = TRUE)
+  },
+  error = function(c) NULL
+  )
+  if(!is.null( mkd)){
+    rmarkdown::render(mkd,
+                      params=list(outDir=outDir),
+                      output_file = "DataChecks.pdf",
+                      output_dir=outDir,
+                      quiet = TRUE)
+  }
+
+
+}
